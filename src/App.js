6e9a9179ecad6b8cab069d829060f47f97890c76
Sobhi Malk <sobhi@cloudulu.com>
2021-05-19 19:48:32 +0200

    test-4

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages";
 import SigninPage from "./pages/signin";
 import { Button, View, Text } from "react-dom";
-import { CookieBot } from "react-cookiebot/lib/CookieBot";
+// import { CookieBot } from "react-cookiebot/lib/CookieBot";
 
-const domainGroupId = "30ee9c14-eff9-45be-9007-a78faa459041";
+// const domainGroupId = "30ee9c14-eff9-45be-9007-a78faa459041";
 
 function App() {
   const [hasCookieBot, setHasCookieBot] = useState(undefined);
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
         <View style={{ flex: 1, alignItems: "center" }}>
-                  <CookieBot domainGroupId={domainGroupId} />
+                  {/* <CookieBot domainGroupId={domainGroupId} /> */}
           <Text>Click to test Cookiebot</Text>
           <Button
             title="TEST"
