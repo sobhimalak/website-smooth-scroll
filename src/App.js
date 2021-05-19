5aa2799d0ed44bc583487130db3445c6d0c5ee31
Sobhi Malk <sobhi@cloudulu.com>
2021-05-19 19:46:46 +0200

    test-3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages";
 import SigninPage from "./pages/signin";
-// import { Button, View, Text } from "react-dom";
-// import { CookieBot } from "react-cookiebot/lib/CookieBot";
+import { Button, View, Text } from "react-dom";
+import { CookieBot } from "react-cookiebot/lib/CookieBot";
 
-// const domainGroupId = "30ee9c14-eff9-45be-9007-a78faa459041";
+const domainGroupId = "30ee9c14-eff9-45be-9007-a78faa459041";
 
 function App() {
-  // const [hasCookieBot, setHasCookieBot] = useState(undefined);
+  const [hasCookieBot, setHasCookieBot] = useState(undefined);
 
   return (
     <Router>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
-        {/* <View style={{ flex: 1, alignItems: "center" }}>
+        <View style={{ flex: 1, alignItems: "center" }}>
                   <CookieBot domainGroupId={domainGroupId} />
           <Text>Click to test Cookiebot</Text>
           <Button
@@ -28,7 +28,7 @@ function App() {
           <Text style={{ color: "red", marginVertical: 10 }}>
             {hasCookieBot && `Has CookieBot: ${JSON.stringify(hasCookieBot)}`}
           </Text>
-        </View> */}
+        </View>
       </Switch>
     </Router>
   );
