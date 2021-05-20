9e127ec34ef3724754a461589863075c5296d345
Sobhi Malk <sobhi@cloudulu.com>
2021-05-20 08:35:45 +0200

    test-7 cookiebot ready

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages";
 import SigninPage from "./pages/signin";
-import { CookieBot } from "react-cookiebot/lib/CookieBot";
-
-const domainGroupId = "30ee9c14-eff9-45be-9007-a78faa459041";
 
 function App() {
   return (
@@ -12,7 +9,6 @@ function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
-        <CookieBot domainGroupId={domainGroupId} />
       </Switch>
     </Router>
   );
